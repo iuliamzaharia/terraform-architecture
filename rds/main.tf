6e54b56f721a9367d776688a9b9c74b5a8b8c315
iuliamzaharia <142277512+iuliamzaharia@users.noreply.github.com>
2023-11-25 15:26:33 -0600

    added remote state

---
+++ rds/main.tf
@@ -0,0 +1,18 @@
+#Pulls information from another workspace
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "iuliamzaharia"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+#Creates subnet group with private subnet 
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [
+    data.terraform_remote_state.vpc.outputs.private_subnets
+    ]
+}
